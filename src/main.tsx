89a533232bfe913e05a63f3d2e6952d0586c7a89
neXaro-Solutions <kontakt@nexaro-solutions.de>
2026-09-21 00:34:24 +0200

    style(theme): load light palette after all legacy styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import "./light.css";
 import { installSelectOnFocus } from "./lib/focusInputs";
 installSelectOnFocus();
 createRoot(document.getElementById("root")!).render(
